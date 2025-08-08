f3429bb755d464194e8e21c14d61290e18c34e1c
nahshon5 <nahshonkyalo5@gmail.com>
2025-08-08 10:35:44 -0700

    Add MYSQL assignment answers.sql

---
+++ answers.sql
@@ -0,0 +1,12 @@
+-- SQL Database Management Assignment
+-- Author: Nahshon Kyalo
+-- Date:2025-08-08
+
+-- Create the salesDB database
+CREATE DATABASE salesDB;
+
+-- Use salesDB
+USE salesDB;
+
+-- Drop database demo
+DROP DATABASE demo;
